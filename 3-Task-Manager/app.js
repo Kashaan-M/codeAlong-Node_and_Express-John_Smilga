802caf3aeb190bf84fb0038c9058b780c8e041c3
Kashaan-M <kashaanmahmood@gmail.com>
2022-11-24 16:33:54 +0500

    Task-Manager-App(working) : Added 404 route handler

--- 3-Task-Manager/app.js
+++ 3-Task-Manager/app.js
@@ -5,6 +5,7 @@ const { resolve } = require('path');
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
 require('dotenv').config();
+const notFound = require('./middleware/not-found');
 
 // logger
 app.use(morgan('tiny'));
@@ -14,7 +15,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // routes
+
 app.use('/api/v1/tasks', tasks);
+// 404 route and middleware on below line for all the routes that don't exist
+app.use(notFound);
 
 const port = 3000;
 
