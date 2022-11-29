3b9b76fa7d239d6afb3df40ecd36f389c3e20afc
Kashaan-M <kashaanmahmood@gmail.com>
2022-11-29 16:09:39 +0500

    5-JWT-Basics: added auth middleware & new error classes

--- 05-JWT-Basics/middleware/auth.js
+++ 05-JWT-Basics/middleware/auth.js
@@ -1,3 +1,28 @@
+const jwt = require('jsonwebtoken');
+const CustomAPIError = require('../errors/custom-error');
+
 const authenticationMiddleware = async (req, res, next) => {
-  console.log();
+  //
+  const authHeader = req.headers.authorization;
+
+  //console.log('authHeader', authHeader, typeof authHeader);
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    // better error message is 'invalid credentials'
+    throw new CustomAPIError('no token provided', 401);
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    //console.log('decoded', decoded);
+    const { id, username } = decoded;
+    req.user = { id, username };
+    next();
+  } catch (error) {
+    throw new CustomAPIError('Not authorized to access this route', 401);
+  }
 };
+
+module.exports = authenticationMiddleware;
--- 05-JWT-Basics/routes/main.js
+++ 05-JWT-Basics/routes/main.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { login, dashboard } = require('../controllers/main');
+const authMiddleware = require('../middleware/auth');
 
 const router = express.Router();
 
-router.get('/dashboard', dashboard);
+router.get('/dashboard', authMiddleware, dashboard);
 router.post('/login', login);
 
 module.exports = router;
