15f963b7c1f7928fe9d37e373461b85063b54141
Kashaan-M <kashaanmahmood@gmail.com>
2022-12-02 09:42:17 +0500

    6-Jobs-Api added jwt in controllers/auth

--- 6-Jobs-API/controllers/auth.js
+++ 6-Jobs-API/controllers/auth.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const { BadRequestError } = require('../errors');
+const jwt = require('jsonwebtoken');
 
 const register = async (req, res) => {
   //if (!name || !email || !password) {
@@ -10,7 +12,9 @@ const register = async (req, res) => {
   /* password hashing with bcryptjs */
 
   const user = await User.create({ ...req.body });
-  res.status(StatusCodes.CREATED).json(user);
+  const token = jwt.sign({ userId: user._id, name: user.name }, 'jwtsecret', { expiresIn: '30d' });
+
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 const login = async (req, res) => {
